fe6cb63ad41949100632cd9bafc2afb7de6c23e3
naveenm666 <78642493+naveenm666@users.noreply.github.com>
2023-10-11 11:32:06 +0530

    first projects
    these are the examples by using js and jquery

---
+++ new.js
@@ -0,0 +1,22 @@
+function show(){
+	document.getElementById('demo').innerHTML='first example';
+}
+function hide(){
+	document.getElementById('demo').innerHTML='';
+}
+   function getValue() {
+
+  var x = document.getElementById("myInput").value;
+  var y = document.getElementById("input").value;
+  document.getElementById("display").innerHTML = x;
+   document.getElementById("show").innerHTML = y;
+
+    }
+    
+      function show() {
+
+         var y= document.getElementById("input").value;
+         document.getElementById("name").innerHTML = y;
+ 
+      
+      }
